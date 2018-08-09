394c68153d333e42dfeece3a576dc5aab69492b8
Cologler <skyoflw@gmail.com>
2018-08-10 02:13:11 +0800

    refactor: use pytest style test

--- tests/test_extras_tinydb.py
+++ tests/test_extras_tinydb.py
@@ -13,7 +13,7 @@ from tinydb import TinyDB
 from tinydb.storages import MemoryStorage
 from lquery.extras.tinydb import TinyDbQuery
 
-def get_example_db():
+def get_example_db_1():
     db = TinyDB(storage=MemoryStorage)
     table = db.table()
     table.insert({'int': 1, 'char': 'a'})
@@ -21,51 +21,33 @@ def get_example_db():
     table.insert({'int': 2, 'char': 'b'})
     return db
 
-class Test(unittest.TestCase):
-    def test_by_field_1_indexstyle(self):
-        db = get_example_db()
-        table = db.table()
-        query = TinyDbQuery(table)
-        self.assertListEqual(
-            query.where(lambda x: x['int'] == 1).to_list(),
-            [{'int': 1, 'char': 'a'}, {'int': 1, 'char': 'b'}]
-        )
+def test_get_items_by_index():
+    db = get_example_db_1()
+    table = db.table()
+    query = TinyDbQuery(table)
+    # field 1
+    assert query.where(lambda x: x['int'] == 1).to_list() == [
+        {'int': 1, 'char': 'a'},
+        {'int': 1, 'char': 'b'}
+    ]
+    # field 2
+    assert query.where(lambda x: x['char'] == 'b').to_list() == [
+        {'int': 1, 'char': 'b'},
+        {'int': 2, 'char': 'b'}
+    ]
 
-    def test_by_field_2_indexstyle(self):
-        db = get_example_db()
-        table = db.table()
-        query = TinyDbQuery(table)
-        self.assertListEqual(
-            query.where(lambda x: x['char'] == 'b').to_list(),
-            [{'int': 1, 'char': 'b'}, {'int': 2, 'char': 'b'}]
-        )
+def test_get_items_by_index_with_attrstyle():
+    db = get_example_db_1()
+    table = db.table()
+    query = TinyDbQuery(table)
+    # provider was rewrite bytecode and change `int` to `['int']`!
+    assert query.where(lambda x: x.int == 1).to_list() == [
+        {'int': 1, 'char': 'a'},
+        {'int': 1, 'char': 'b'}
+    ]
 
-    def test_by_doc_id(self):
-        db = get_example_db()
-        table = db.table()
-        query = TinyDbQuery(table)
-        self.assertListEqual(
-            query.where(lambda x: x.doc_id == 2).to_list(),
-            [{'int': 1, 'char': 'b'}]
-        )
-
-    def test_by_field_1_attrstyle(self):
-        # provider was rewrite bytecode and change `int` to `['int']` !
-        db = get_example_db()
-        table = db.table()
-        query = TinyDbQuery(table)
-        self.assertListEqual(
-            query.where(lambda x: x.int == 1).to_list(),
-            [{'int': 1, 'char': 'a'}, {'int': 1, 'char': 'b'}]
-        )
-
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv
-    try:
-        unittest.main()
-    except Exception:
-        traceback.print_exc()
-
-if __name__ == '__main__':
-    main()
+def test_get_items_by_doc_id():
+    db = get_example_db_1()
+    table = db.table()
+    query = TinyDbQuery(table)
+    assert query.where(lambda x: x.doc_id == 2).to_list() == [{'int': 1, 'char': 'b'}]
